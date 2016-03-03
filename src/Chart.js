a5379771502def35e138ce45fe2735f946cfc9a6
jianghai <jianghai1007@gmail.com>
2016-03-03 13:39:37 +0800

    update

--- src/Chart.js
+++ src/Chart.js
@@ -12,26 +12,15 @@ export default React.createClass({
     this.renderChart(res)
   },
 
-  getParentContentHeight() {
-    const style = window.getComputedStyle(this.refs.container.parentNode)
-    let extra = 0
-
-    if (style.boxSizing === 'border-box') {
-        extra = parseInt(style.paddingTop, 10) + parseInt(style.paddingBottom, 10) + parseInt(style.borderTopWidth, 10) + parseInt(style.borderBottomWidth, 10)
-    }
-    return parseInt(style.height, 10) - extra
-  },
-
   componentDidMount() {
-    const height = this.getParentContentHeight()
-    this.refs.container.style.height = this.refs.chart.style.height = height + 'px'
+    this.refs.container.style.height = this.refs.chart.style.height = '100%'
   },
 
   render() {
     return (
-      <div ref="container" className={this.props.className}>
+      <div ref="container">
         <Loading url={this.props.url} onSuccess={this.handleSuccess}></Loading>
-        <div ref="chart"></div>
+        <div ref="chart" className={this.props.className}></div>
       </div>
     )
   }
