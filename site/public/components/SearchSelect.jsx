0026c4e117daef9ce56f4dc802b4db9642f2cfaa
zhangyu <313283928@qq.com>
2016-03-30 15:45:22 +0800

    SearchSelect完善并支持2种接口数据

--- site/public/components/SearchSelect.jsx
+++ site/public/components/SearchSelect.jsx
@@ -19,7 +19,7 @@ const App = React.createClass({
 
   render() {
     return (
-        <SearchSelect url="/data/searchSelect.json?search=" onChange={this.handleChange}></SearchSelect>      
+        <SearchSelect url="/data/searchSelect.json" onChange={this.handleChange}></SearchSelect>      
     )
   }
 })
@@ -59,7 +59,9 @@ const App = React.createClass({
         <Props>
           <Prop name="Data" required>
             <p>返回数据格式</p>  
+            <h5>键值对数据格式：</h5>
             <Pre>
+          
 {`
 {
   "code": 200,
@@ -90,18 +92,27 @@ const App = React.createClass({
   }]
 }
 `}
-        </Pre>  
-      	</Prop>
-      	<Prop name="url" required>
-        	<p>请求接口</p>  
-      	</Prop>
-     	<Prop name="onChange" type="Function" required>
-        	<p>选择后的回调</p>    
-      	</Prop>
+            </Pre>  
+            <h5>字符串数据格式：</h5>
+            <Pre>
+{`
+{
+  "code": 200,
+  "data": ["小米1","小米2","小米3","小米4","三星1","三星2","三星3","三星4","乐视1","乐视2","乐视3"]
+}
+`}
+            </Pre>  
+          	</Prop>
+          	<Prop name="url" required>
+            	<p>请求接口</p>  
+          	</Prop>
+         	<Prop name="onChange" type="Function" required>
+            	<p>选择后的回调</p>    
+          	</Prop>
 
-        </Props>    
-         
-      </div>
+            </Props>    
+             
+          </div>
     )
   }
 })
--- src/SearchSelect/index.jsx
+++ src/SearchSelect/index.jsx
@@ -9,62 +9,124 @@ const SearchSelect = React.createClass({
 
 	getInitialState() {
 		return {
-			url: this.props.url,
+			url: this.props.url,			
 			data: [],
+			searchData:[],
 			result:[],
+			type:null,
 			selected:false,
 			disabled:this.props.disabled,
 		}
 	},
-	
-	handleChange(e) {		
-		this.setState({
-			url: this.props.url + e.target.value
-		})
+
+	handleSuccess(data) {
+		if(this.getType(data,'string')){
+			this.setState({type:'string'});
+		} 
+		if(this.getType(data,'object')){
+			this.setState({type:'object'});
+		}
+		this.setState({	data: data,	searchData: data });
 	},
 
-	handleClick(item) {
+	getType(data, type) {
+		let flag = true;
+		data.map(function(item) {
+			if (typeof item !== type) {
+				flag = false;
+			}
+		})
+		return flag;
+	},
+	
+	handleChange(e) {			
+		const array = this.state.data,
+			keyword = e.target.value;
+		let data = [];
+		switch (this.state.type) {
+			case 'string':
+				array.map(function(item){
+					if(item.indexOf(keyword) != -1){
+						data.push(item);
+					}
+				});
+				break;
+			case 'object':
+				array.map(function(item){
+					if(item.value.indexOf(keyword) != -1){
+						data.push(item);
+					}
+				});
+				break;
+			default:				
+				break;
+		}					
+		this.setState({ searchData: data });						
+	},
+
+	handleClick(item) {		
 		let arr = this.state.result;
 		if (this.state.result.indexOf(item) == -1) arr.push(item);					
 		this.state.result.length>0 ? this.setState({selected:true,disabled:true}) : this.setState({selected:false,disabled:false});
 		this.props.onChange(arr);
-		this.setState({result:arr});		
+		this.setState({result:arr});					
 	},
 
-	handleRemove(obj){		
-		let arr = [];		
-		this.state.result.forEach(item => {
-        	if(obj.key !== item.key)
-			arr.push(item);
-      	});
+	handleRemove(obj){	
+		let arr = this.state.result;
+			arr.splice(arr.indexOf(obj),1);
 		if (arr.length == 0)  this.setState({selected:false,disabled:false});
 		this.props.onChange(arr);
-		this.setState({result:arr});		
+		this.setState({result:arr});				
 	},
-
-	handleSuccess(data) {
-		this.setState({	data });	
-	},
-
-	render() {
-		const children = this.state.data.map(item => {			
+	render() {		
+		const children = this.state.searchData.map(item => {			
 			const classname = classNames({
 				'active': this.state.result.indexOf(item) != -1
 			});
-			return ( 
-				<li value={item.key} key={item.key} onClick={this.handleClick.bind(this,item)} className={classname}> 
-					{item.value} 
-				</li> 
-			);
+			switch (this.state.type) {
+				case 'string':
+					return ( 
+						<li value={item} key={item} onClick={this.handleClick.bind(this,item)} className={classname}> 
+							{item} 
+						</li> 
+					);
+					break;
+				case 'object':
+					return ( 
+						<li value={item.key} key={item.key} onClick={this.handleClick.bind(this,item)} className={classname}> 
+							{item.value} 
+						</li> 
+					);
+					break;
+				default:
+					return null;
+					break;
+			}
 		});
-		const _result = this.state.result.map(item=>{
-			return ( 
-				<li value={item.key} key={item.key}>
-					<i onClick={this.handleRemove.bind(this,item)} className="glyphicon glyphicon-remove"></i>
-					{item.value}
-				</li> 
-			);
-		})
+		const _result = this.state.result.map(item=>{				
+			switch (this.state.type) {
+				case 'string':
+					return ( 
+						<li value={item} key={item}>
+							<i onClick={this.handleRemove.bind(this,item)} className="glyphicon glyphicon-remove"></i>
+							{item}
+						</li> 
+					);
+					break;
+				case 'object':
+					return ( 
+						<li value={item.key} key={item.key}>
+							<i onClick={this.handleRemove.bind(this,item)} className="glyphicon glyphicon-remove"></i>
+							{item.value}
+						</li> 
+					);
+					break;
+				default:
+					return null;
+					break;
+			}
+		});	
 		return (				
 			<Dropdown ref="searchSelect" className="bfd-search-select" disabled={this.state.disabled}>
 		        <DropdownToggle>
