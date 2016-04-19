1cf6a6a7073654fd90d56b3049b9dba671eca682
jianghai <jianghai1007@gmail.com>
2016-04-19 10:49:03 +0800

    patch

--- src/Percentage/index.jsx
+++ src/Percentage/index.jsx
@@ -1,16 +1,25 @@
 import React, { PropTypes } from 'react'
-import Percentage from './main'
+import PercentageChart from './main'
+import classnames from 'classnames'
 import './main.less'
 
-export default React.createClass({
+const propTypes = {
+  percent: PropTypes.number.isRequired,
+  foreColor: PropTypes.string,
+  backColor: PropTypes.string,
+  textColor: PropTypes.string,
+  customProp({ percent }) {
+    if (percent < 0 || percent > 100) {
+      return new Error('percent 必须为 0 到 100 之间的数字')
+    }
+  }
+}
 
-  propTypes: {
-    percent: PropTypes.number.isRequired
-  },
+const Percentage = React.createClass({
   
   renderChart() {
     const config = {container: this.refs.container, ...this.props}  
-    new Percentage(config)
+    new PercentageChart(config)
   },
 
   componentDidMount() {
@@ -18,15 +27,18 @@ export default React.createClass({
   },
 
   shouldComponentUpdate(nextProps) {
-    if (this.props.data !== nextProps.data) {
+    if (this.props.percent !== nextProps.percent) {
       this.renderChart()
     }
     return true
   },
 
   render() {
-    return <div ref="container" className="bfd-percentage"></div>
+    const { className, ...other } = this.props
+    return <div ref="container" className={classnames('bfd-percentage', className)} {...other}></div>
   }
 })
 
+Percentage.propTypes = propTypes
 
+export default Percentage
