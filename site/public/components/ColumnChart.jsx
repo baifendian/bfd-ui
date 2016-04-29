3c5b01eafc27f2845d76a02b83d5965a94cfd4e1
jiangtl <274351128@qq.com>
2016-04-29 14:57:30 +0800

    ColumnChart

--- site/public/components/ColumnChart.jsx
+++ site/public/components/ColumnChart.jsx
@@ -7,7 +7,7 @@ const code = `import ColumnChart from 'bfd-ui/lib/ColumnChart'
 
 const App = React.createClass({
   render() {
-    return <ColumnChart style={{height: 400}} category="name" cols={{x: '日环比新增', y: '系列环比新增'}} url="/data/columnChart.json"/>
+    return <ColumnChart style={{height: 400}} category="name" cols={{x: '日环比新增', y: '系列新增', z: '进店新增', a: '消费新增'}} url="/data/columnChart.json"/>
   }
 })`
 
@@ -34,21 +34,15 @@ export default React.createClass({
 {`{
   x: '日环比新增', 
   y: '系列环比新增',
-  z: '进店环比新增'
+  z: '进店环比新增',
+  a: '消费新增'
 }`}
             </Pre>
           </Prop>
           <Prop name="category" type="String" required>
-            <p>x轴字段名，比如 'date'</p>
+            <p>x轴字段名，比如 'name'</p>
           </Prop>
-          <Prop name="yAxis" type="Object">
-            <p>y轴相关配置</p>
-            <Pre>
-{`{
-  format: '%', // Y字段值格式，默认千分符 's' 
-}`}
-            </Pre>
-          </Prop>
+          
           <Prop name="url" type="String">
             <p>数据源URL，data 格式要求数组，字段分别对应 cols 和 category</p>
             <Pre>
@@ -56,10 +50,11 @@ export default React.createClass({
   "code": 200,
   "message": "当code不是200时，message信息会展示到页面",
   "data": [{
-    x: 100,
-    y: 3432,
-    z: 660,
-    name: "2016-01-01"
+    x: 234,
+    y: 255,
+    z: 366,
+    a: 198
+    name: "01-01"
   }]
 }`}
             </Pre>
@@ -68,10 +63,11 @@ export default React.createClass({
             <p>数据源，格式与 url 方式返回的 data 格式一样</p>
             <Pre>
 {`[{
-  x: 100,
-  y: 3432,
-  z: 161,
-  name: "2016-01-01"
+  x: 234,
+  y: 255,
+  z: 366,
+  a: 198
+  name: "01-01"
 }]`}
             </Pre>
             <div className="alert alert-warning">url 和 data 属性至少提供一个</div>
