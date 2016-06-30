159577a5fe6cc8485ef3961d3f1aa578c7922b4f
jianghai <jianghai1007@gmail.com>
2016-06-30 10:21:53 +0800

    remove nav test

--- src/Nav/__tests__/index.jsx
+++ src/Nav/__tests__/index.jsx
@@ -1,45 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
+import { Router, Route, IndexRoute } from 'react-router'
 import { Nav, NavItem } from '../index'
 
-describe('Nav', () => {
+// TestUtils.renderIntoDocument({
+  
+// })
 
-  it('index is ok', () => {
-    const nav = TestUtils.renderIntoDocument((
-      <Nav href="/">
-        <NavItem />
-      </Nav>
-    ))
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[0].href).toBe('/')
-  })
+// describe('Nav', () => {
 
-  it('child href is ok', () => {
-    const nav = TestUtils.renderIntoDocument((
-      <Nav href="/">
-        <NavItem href="test" />
-      </Nav>
-    ))
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[0].href).toBe('/test')
-  })
+//   it('index is ok', () => {
+//     const nav = TestUtils.renderIntoDocument((
+//       <Nav href="/">
+//         <NavItem />
+//       </Nav>
+//     ))
+//     expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[0].href).toBe('/')
+//   })
 
-  it('Nav no href is ok', () => {
-    const nav = TestUtils.renderIntoDocument((
-      <Nav>
-        <NavItem href="/test" />
-      </Nav>
-    ))
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[0].href).toBe('/test')
-  })
+//   it('child href is ok', () => {
+//     const nav = TestUtils.renderIntoDocument((
+//       <Nav href="/">
+//         <NavItem href="test" />
+//       </Nav>
+//     ))
+//     expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[0].href).toBe('/test')
+//   })
 
-  it('child NavItem href is ok', () => {
-    const nav = TestUtils.renderIntoDocument((
-      <Nav href="/">
-        <NavItem href="test">
-          <NavItem href="test/aa" />
-        </NavItem>
-      </Nav>
-    ))
-    expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[1].href).toBe('/test/aa')
-  })
-})
+//   it('Nav no href is ok', () => {
+//     const nav = TestUtils.renderIntoDocument((
+//       <Nav>
+//         <NavItem href="/test" />
+//       </Nav>
+//     ))
+//     expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[0].href).toBe('/test')
+//   })
+
+//   it('child NavItem href is ok', () => {
+//     const nav = TestUtils.renderIntoDocument((
+//       <Nav href="/">
+//         <NavItem href="test">
+//           <NavItem href="test/aa" />
+//         </NavItem>
+//       </Nav>
+//     ))
+//     expect(TestUtils.scryRenderedDOMComponentsWithTag(nav, 'a')[1].href).toBe('/test/aa')
+//   })
+// })
