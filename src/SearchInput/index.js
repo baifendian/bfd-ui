782d1bbf439728d6feae2ad2755d0fa73c2dd442
jiangtl <274351128@qq.com>
2016-08-08 10:23:20 +0800

    SearchInput add defaultValue props

--- src/SearchInput/index.js
+++ src/SearchInput/index.js
@@ -9,28 +9,34 @@ import ClearableInput from '../ClearableInput'
 import classnames from 'classnames'
 
 export default React.createClass({
+
   value: '',
+
   propTypes: {
     onSearch: React.PropTypes.func.isRequired
   },
+
   handleChange(v) {
     this.value = v
     this.props.onChange && this.props.onChange(v)
   },
+
   handleClick() {
     if (typeof this.props.onSearch == 'function') {
       this.props.onSearch(this.value);
     }
   },
+
   render() {
     const {
       className,
       ...other
     } = this.props
     const size = this.props.size || 'lg'
+    this.value = this.props.defaultValue || ''
     return (
       <div className={classnames('bfd-search_input', className, size)} {...other}>        
-        <ClearableInput size={size} onChange={this.handleChange} inline placeholder={this.props.placeholder || '' }/>
+        <ClearableInput defaultValue={this.value} size={size} onChange={this.handleChange} inline placeholder={this.props.placeholder || '' }/>
         <button className={classnames('btn btn-primary', size)} type="button" onClick={this.handleClick}>
           <span className="glyphicon glyphicon-search"></span>
           {this.props.label || '搜索'} 
