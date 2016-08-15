bbfcb8e310f0163aeba3c74ef49f5adbd9b9989c
jiangtl <274351128@qq.com>
2016-08-15 19:20:39 +0800

    add doc

--- src/DataTable/index.js
+++ src/DataTable/index.js
@@ -300,9 +300,35 @@ class DataTable extends Component {
   }
 }
 
-DataTable. propTypes = {
-  data: PropTypes.object,
+DataTable.propTypes = {
+
+  // 要请求数据的服务端地址。
   url: PropTypes.string,
+
+  // 数据表格表头列名称
+  column: PropTypes.array.isRequired,
+
+  // 每页需要显示的条数
+  howRow: PropTypes.number,
+
+  // DataTable显示数据，选填，url和data属性二者之间必须有一个，不必同时出现。data支持一次性查询多条数据传入data属性中，不需要点击一次分页再发送一次ajax请求，此功能根据业务需求使用！
+  data: PropTypes.object,
+
+  // 是否显示分页，true为显示，false为不显示,如果showPage设置为false，就要同时取消howRow每页显示多少条的设置
+  showPage: PropTypes.string,
+
+  // 点击分页时回调函数，此回调方法是点击切换分页时触发，可以在此方法体内发送Ajax请求数据，来替代组件的url属性！注【如果组件加入此属性方法，则不可以再传入url属性】
+  onPageChange: PropTypes.func,
+
+  // 复选框点击事件，返回被选中的行记录
+  onCheckboxSelect: PropTypes.func,
+
+  // 行点击事件，返回被选中的行记录
+  onRowClick: PropTypes.func,
+
+  // 列名称点击排序事件，返回列名称和排序状态
+  onOrder: PropTypes.func,
+
   customProp({ data, url }) {
     if (data && url) {
       return new Error('data属性和url属性不能同时使用！')
--- src/StackedColumnChart/index.js
+++ src/StackedColumnChart/index.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import Chart from '../Chart'
 import ColumnChart from './main'
 import classnames from 'classnames'
 import './main.less'
 
-export default React.createClass({
+class chart extends Component {
   render() {
     const { className, ...other } = this.props
     return <Chart type={ColumnChart} className={classnames('bfd-column-chart', className)} {...other} />
   }
-})
+}
+
+chart.propTypes = {
+
+  // y轴字段配置
+  cols: PropTypes.object.isRequired,
+
+  // x轴字段名
+  category: PropTypes.string.isRequired,
+
+  // 数据源URL，data 格式要求数组，字段分别对应 cols 和 category
+  url: PropTypes.string,
+
+  // 数据源，格式与 url 方式返回的 data 格式一样
+  data: PropTypes.array,
+
+}
+
+export default chart
